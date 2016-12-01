e290ab3f2fb1a37cc8349cd7b09893d9882eb106
huuson235 <huuhoangson235@gmail.com>
2016-12-02 02:23:31 +0700

    new feature
    choose operator

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function (str) { 
+module.exports = function (str, operator) { 
   str = str
     .toLowerCase()
     .replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a")
@@ -10,8 +10,9 @@ module.exports = function (str) {
     .replace(/đ/g, "d")
     .replace(/\s+/g, "-")
     .replace(/[^A-Za-z0-9_-]/g, "")
-    .replace(/-+/g, "-")
+    .replace(/-+/g, "-");
+  if (operator) {
+    return str.replace(/-/g, operator);
+  }
   return str;
 }
-// console.log('abc');
-// console.log(removeVietnameseMark("[Phần 1] - Xin chào, tôi tên là Sơn!"));
